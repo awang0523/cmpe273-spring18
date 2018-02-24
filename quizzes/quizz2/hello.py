407a59b6287fe36cabc5200e386e2dcc30a693d4
wangannan <wangannan@wangannantekiMacBook-Air.local>
2018-02-23 16:46:16 -0800

    edited hello.py

--- quizzes/quizz2/hello.py
+++ quizzes/quizz2/hello.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
+names = {}
+uid = 1
 
 @app.route('/', methods=['GET'])
 def hello():
@@ -9,5 +12,18 @@ def hello():
 
 @app.route('/users', methods=['POST'])
 def new_users():
+    global uid
     name = request.form["name"]
-    return "Hello {}!".format(name)
+    names[uid] = name
+    result = jsonify({'id': uid, 'name': name})
+    uid = uid + 1
+    return result, 201
+
+@app.route('/users/<int:uid>', methods=['GET', 'DELETE'])
+def user(uid):
+    if request.method == 'GET':
+        result = jsonify({'id': uid, 'name': names[uid]})
+        return result
+    elif request.method == 'DELETE':
+        names.pop(uid, None)
+        return '', 204
